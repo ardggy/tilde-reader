68aca445bd897422ee7563b8a3c86b7b181f6446
toshiabe <toshihisa.abe@gmail.com>
2011-11-29 00:02:29 +0900

    modify eval-when form.

--- t/test.lisp
+++ t/test.lisp
@@ -7,28 +7,29 @@
     (tilde-reader:install)
 
     (multiple-value-bind (func non-term-p) (get-macro-character #\~)
-      (assert-eql (values t t) (values (functionp func) non-term-p)))))
+      (assert-eql (values #'tilde-reader.reader::tilde-reader t)
+                  (values func non-term-p)))))
 
 (define-test #:uninstall
-  (let ((*readtable* (copy-readtable)))
+  (let ((*readtable* (copy-readtable nil)))
+    (tilde-reader:install)
     (tilde-reader:uninstall)
 
   (multiple-value-bind (func non-term-p) (get-macro-character #\~)
     (assert-eql (values nil nil) (values func non-term-p)))))
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (tilde-reader:install))
 
-(let ((*readtable* (copy-readtable nil)))
-  (eval-when (:compile-toplevel :load-toplevel :execute)
-    (tilde-reader:install))
+(define-test #:lambda.0
+  (assert-eql nil (funcall ~(lambda () t))))
 
-  (define-test #:lambda.0
-    (assert-eql nil (funcall ~(lambda () t))))
+(define-test #:lambda.1
+  (assert-eql t (funcall ~#'(lambda () nil))))
 
-  (define-test #:lambda.1
-    (assert-eql t (funcall ~(lambda () nil))))
+(define-test #:named-function
+  (assert-equal '(nil) (remove-if ~#'null '(t nil))))
 
-  (define-test #:named-function
-    (assert-equal '(nil) (remove-if ~#'null '(t nil))))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (tilde-reader:uninstall))
 
-  (eval-when (:compile-toplevel :load-toplevel :execute)
-    (tilde-reader:uninstall)))
